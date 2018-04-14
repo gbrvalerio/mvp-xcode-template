4b064fb137637de7a0f4998d867d0017bc551682
Gabriel Bezerra Valério <gbrvalerio@gmail.com>
2018-04-14 12:44:05 -0300

    Initial commit

---
+++ MVPTemplate.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,39 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+  
+    private weak var view:___VARIABLE_sceneName:identifier___PresenterView?
+  
+    init(view:___VARIABLE_sceneName:identifier___PresenterView) {
+        self.view = view
+    }
+    
+    func viewDidLoad() {
+        
+    }
+    
+    func viewWillAppear() {
+        
+    }
+    
+    func viewDidAppear() {
+        
+    }
+    
+    func viewWillDisappear() {
+        
+    }
+    
+    func viewDidDisappear() {
+        
+    }
+    
+}
